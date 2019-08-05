859cf0a1a4e23e487639c0fbff4e3e9efd928d0b
xtrp <36894883+xtrp@users.noreply.github.com>
2019-08-05 00:49:26 -0400

    wrote php for main files, and more

---
+++ home.php
@@ -0,0 +1,23 @@
+<!-- define preset PHP variables -->
+<?php $pagefile = "home"; $pagetitle = "Home"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <!-- include HEAD tag -->
+    <?php include "assets/html/head.php" ?>
+    <body>
+        <!-- include navbar -->
+        <?php include "assets/html/nav_auth.php" ?>
+
+        <!-- site content and container DIV -->
+        <div class="container">
+
+        </div>
+
+        <!-- include footer -->
+        <?php include "assets/html/footer.php" ?>
+
+        <!-- include scripts -->
+        <?php include "assets/html/scripts.php" ?>
+    </body>
+</html>
