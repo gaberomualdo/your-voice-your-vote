09dc5cd24a405fba9d93ba436903131a6144ead9
xtrp <36894883+xtrp@users.noreply.github.com>
2019-08-05 11:51:40 -0400

    commented scripts incude

--- assets/html/scripts.php
+++ assets/html/scripts.php
@@ -1,2 +1,4 @@
+<!-- this include file imports all necessary scripts for the page -->
+
 <script src="assets/js/script.js"></script>
 <script src="assets/js/pages/<?php echo $pagefile ?>.js"></script>
