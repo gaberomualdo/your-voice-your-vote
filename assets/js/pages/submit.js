207243c0a3bbd73d190f89c0b808d9d8bf3b3421
xtrp <36894883+xtrp@users.noreply.github.com>
2019-08-12 14:15:31 -0400

    a few minor bug fixes, and finished main part of submission form

--- assets/js/pages/submit.js
+++ assets/js/pages/submit.js
@@ -3,14 +3,17 @@
 // display animations when page is loaded
 window.addEventListener("load", () => {
     // header animation
-    document.querySelector("body > div.container > div.block:first-child > div.top_area > h1.header").classList.add("animation_shown");
+    document.querySelector("body > div.container > div.block:first-child > div.row:first-child > h1.header").classList.add("animation_shown");
 
     // description and form animation (in) goes after header animation is done (can be checked with last word transition time)
-    const headerTransitionTimeMilliseconds = parseInt(document.querySelector("body > div.container > div.block:first-child > div.top_area > h1.header > span.word_container:last-child > span.word_animated").style.transitionDelay.split("s")[0]) + 0.6;
+    const headerTransitionTimeMilliseconds = parseInt(document.querySelector("body > div.container > div.block:first-child > div.row:first-child > h1.header > span.word_container:last-child > span.word_animated").style.transitionDelay.split("s")[0]) + 0.6;
 
     setTimeout(() => {
         // animate (in) description and form
-        document.querySelector("body > div.container > div.block:first-child > div.top_area > h1.header ~ div.description").style.opacity = "1";
-        document.querySelector("body > div.container > div.block:first-child > div.top_area > h1.header ~ div.description").classList.add("fadeIn");
+        document.querySelector("body > div.container > div.block:first-child > div.row:nth-child(2) > div.description").style.opacity = "1";
+        document.querySelector("body > div.container > div.block:first-child > div.row:nth-child(2) > div.description").classList.add("fadeIn");
+
+        document.querySelector("body > div.container > div.block:first-child > div.row:nth-child(2) > div.form").style.opacity = "1";
+        document.querySelector("body > div.container > div.block:first-child > div.row:nth-child(2) > div.form").classList.add("fadeIn");
     }, headerTransitionTimeMilliseconds * 1000);
 });
--- submit.php
+++ submit.php
@@ -12,7 +12,8 @@
         <!-- site content and container DIV -->
         <div class="container">
             <div class="block">
-                <div class="top_area">
+                <!-- first row includes just the header -->
+                <div class="row">
                     <!-- header has an animation on each word, so each word is separated (into <span> tags)
                     using PHP, as well as added transition duration for the effect -->
                     <h1 class="header">
@@ -26,9 +27,12 @@
                         })();
                         ?>
                     </h1>
-
+                </div>
+                
+                <!-- second row is split into two columns — the description, and the actual submission form -->
+                <div class="row">
                     <!-- animated (in) description -->
-                    <div class="description animated" style="opacity: 0;">
+                    <div class="col description animated" style="opacity: 0;">
                         <p><strong>Some guidelines for writing a clear and effective proposal:</strong></p>
                         <ul>
                             <li>Lorem Ipsum</li>
@@ -42,13 +46,20 @@
                             <li>Lorem Ipsum</li>
                         </ul>
                     </div>
-                </div>
-                <div class="form">
-                    <input type="text" class="title" placeholder="Write a title...">
-                    <textarea placeholder="Write a detailed description..." rows="12"></textarea>
-                    <div class="button_container">
-                        <button>Submit Proposal for Approval</button>
-                        <div class="submit_anon_checkbox"><input type="checkbox"><p>Submit Anonymously</p></div>
+                
+                    <!-- submission form -->
+                    <div class="col form animated" style="opacity: 0;">
+                        <!-- title box -->
+                        <input type="text" class="title" placeholder="Write a title..." spellcheck="false">
+                        
+                        <!-- description box -->
+                        <textarea placeholder="Write a detailed description..." spellcheck="false"></textarea>
+                        
+                        <!-- submit button with "Submit Anonymously" checkbox -->
+                        <div class="button_container">
+                            <button>Submit Proposal for Approval</button>
+                            <div class="submit_anon_checkbox"><input type="checkbox"><p>Submit Anonymously</p></div>
+                        </div>
                     </div>
                 </div>
             </div>
